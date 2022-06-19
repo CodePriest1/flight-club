b52c544c8c4a80052a5bbfa68ff10a548518712c
CodePriest1 <88582897+CodePriest1@users.noreply.github.com>
2022-06-19 12:35:53 +0100

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+#You will get your individual ACCOUNT SID and ACCOUNT TOKEN when you sign upmto Twilio using the link in https://www.twilio.com/docs/sms 
 ACCOUNT_SID = "SID"
 ACCOUNT_TOKEN ="TOKEN"
 PHONE_NUMBER = "PHONE NUMER"
