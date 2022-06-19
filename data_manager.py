9047a897ce0d2708abd3796d65cbac2129cf3c31
CodePriest1 <88582897+CodePriest1@users.noreply.github.com>
2022-06-19 12:27:10 +0100

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -5,7 +5,8 @@ SHEET_GET = "SHEET GET"
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     pass
-
+    
+    # This is to get the data in the sheet from sheety
     def get_sheet_data(self):
         response = requests.get(url=SHEET_GET)
         response.raise_for_status()
@@ -13,6 +14,7 @@ class DataManager:
         self.sheet_data = data["prices"]
         return self.sheet_data
 
+    #This is to pass the IATACODE to the sheet in sheety using the PUT for each of the countries
     def update_code(self):
         for row in self.sheet_data:
             new_data = {
